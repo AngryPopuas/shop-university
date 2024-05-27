0d0233415d2c4bfe21ec2ff470dab658e92d4a22
денис цыганков <alaxababa@mail.ru>
2024-05-27 18:10:58 +0500

    second commit

--- components/business/products/product-cart/product-cart-list/ProductCartList.tsx
+++ components/business/products/product-cart/product-cart-list/ProductCartList.tsx
@@ -5,9 +5,9 @@ import ProductCartItem from "../product-cart-item/ProductCartItem"
 const ProductCartList = ({ products }: { products: Array<IProductCart> }) => {
     return (
         <div className="flex flex-col space-y-5 w-full">
-            {products.map((product) => {
+            {products.map((product, index) => {
                 return (
-                    <ProductCartItem props={product} />
+                    <ProductCartItem props={product} key={index} />
                 )
             })}
         </div>
